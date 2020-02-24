929df71120cf46b7f9574742b7e736f56e9fb643
Sunic <gitsunic@mail.ru>
2020-02-24 21:46:27 +0300

    flex bot on GitHub

---
+++ cfg.py
@@ -0,0 +1,6 @@
+# discord_token = '<токен дискорд приложения>'
+# genius_token = 'токен для Genius API' - для функции .text
+# youtube_token = [список токенов для youtube API v3]
+# imgbb_token = 'imgbb токен' - для функции .upload
+# version = '<версия продукта>'
+# me = <твой дискорд ID> - для функции .bug
